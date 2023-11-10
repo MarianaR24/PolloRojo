6d5d2dce42aa7135e32854b3367daa0b4dc1e968
Mariana Alejandra Rueda Duarte <marianadu202@gmail.com>
2023-11-10 17:04:15 -0500

    Auto stash before merge of "main" and "origin/main"

--- Assets/Scripts/DialogueController.cs
+++ Assets/Scripts/DialogueController.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueController : MonoBehaviour
 {
-    void Start()
-    {
-        
+    public Text dialogueText;
+    public SpriteRenderer dialogueBox;
 
+    private string[] dialogueLines;
+    private int currentLine;
+
+    private void Start()
+    {
+        dialogueLines = new string[] { "Line 1", "Line 2", "Line 3" };
+        currentLine = 0;
+        dialogueBox.gameObject.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space) && currentLine < dialogueLines.Length)
+        {
+            dialogueBox.gameObject.SetActive(true);
+            dialogueText.text = dialogueLines[currentLine];
+            currentLine++;
+        }
+        else if (currentLine >= dialogueLines.Length)
+        {
+            dialogueBox.gameObject.SetActive(false);
+        }
     }
 }
+
